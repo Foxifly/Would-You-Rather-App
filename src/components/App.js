5d947f9bc317c77e10cfe57308be1e7668797136
Lindsay Ciastko <LindsayCiastko@gmail.com>
2019-03-24 15:57:16 -0700

    Set up Nav component for the navigation bar.

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,10 @@ import React, { Component } from "react";
 import handleInitialData from "../actions/shared";
 import { connect } from "react-redux";
 import LoginScreen from "./LoginScreen";
-import "../style/app.css"
+import "../style/app.css";
+import Nav from "./Nav";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 
 class App extends Component {
   componentDidMount() {
@@ -11,6 +14,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Nav/>
         <LoginScreen />
       </div>
     );
