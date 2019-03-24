ca553b60b8b3ecd3bcabf4b58820ff9af02d6512
Lindsay Ciastko <LindsayCiastko@gmail.com>
2019-03-24 06:26:27 -0700

    Connected App with index and created the store.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import handleInitialData from "../actions/shared";
+import { connect } from "react-redux";
 import LoginScreen from "./LoginScreen";
 
 class App extends Component {
@@ -15,4 +16,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect()(App);
