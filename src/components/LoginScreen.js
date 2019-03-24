821cea6e2f9faf848297f3a512b582b9fc39e2c1
Lindsay Ciastko <LindsayCiastko@gmail.com>
2019-03-24 10:38:46 -0700

    Edited select form and connected it to values

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -1,23 +1,64 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class LoginScreen extends Component {
+  state = {
+    id: "",
+    isBlank: null
+  };
+  handleSubmit = e => {
+    console.log("The login id auth: ", this.state.id);
+    e.preventDefault();
+    if (this.state.id) {
+      this.setState(() => ({
+        isBlank: true
+      }));
+    } else {
+      this.setState(() => ({
+        isBlank: false
+      }));
+    }
+  };
+
+  handleChange = e => {
+    const id = e.target.value;
+    if (id) {
+      this.setState(() => ({
+        id
+      }));
+    } else {
+      this.setState(() => ({
+        id
+      }));
+    }
+  };
   render() {
     return (
       <div>
         <h1>Welcome to the would you rather app!</h1>
         <h2>Please sign in to continue</h2>
 
-        <form className="login-form">
-          <select>
-            <option value="volvo">Volvo</option>
-            <option value="saab">Saab</option>
-            <option value="mercedes">Mercedes</option>
-            <option value="audi">Audi</option>
+        <form onSubmit={this.handleSubmit} className="login-form">
+          <select onChange={this.handleChange}>
+            <option />
+            <option value="sarahedo">Sarah Edo</option>
+            <option value="tylermcginnis">Tyler McGinnis</option>
+            <option value="johndoe">John Doe</option>
           </select>
+          <button type="submit"> Submit </button>
         </form>
+
+        {this.state.isBlank === false ? (
+          <p> Please select a valid user </p>
+        ) : null}
       </div>
     );
   }
 }
 
-export default LoginScreen;
+/*
+Right now, it is hardcoded into the app to take the authed id from a hardcoded value.
+On this login screen, it needs to update the authed id to the new authed id.
+*/
+
+export default connect()(LoginScreen);
