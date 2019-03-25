e642a81c47ee076c98a69c5894d66d47e70432a7
Lindsay Ciastko <LindsayCiastko@gmail.com>
2019-03-24 20:56:32 -0700

    I over-thought on the components. Only need one question component

---
+++ src/components/Question.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Question extends Component {
+
+  render() {
+    return (
+      <div>
+        Question
+      </div>
+    );
+  }
+}
+
+export default Question;
